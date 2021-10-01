bf59f51941cdae561b7bc4996f8b81458eae2f43
Joseph Dickens <josephdickens87@gmail.com>
2021-09-30 21:12:24 -0400

    rough working first draft

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const vorpal = require('vorpal')();
-let holeCards = null, board = null, turn = null, river = null;
+const { createVerify } = require('crypto');
+const fs = require('fs');
+const { get } = require('http');
+const path = require('path');
+const chalk = require('chalk')
+
+let holeCards = null, board = null, turn = null, river = null, position = null, file = null;
 
 vorpal
   .delimiter('poker-cli$')
   .show();    
 
 vorpal
-  .command('hc <holeCards>', 'pass hole cards as argument')
+  .command('h <holeCards> <position>',  'pass hole cards as argument')
   .action(function(args, _cb) {
     holeCards = args.holeCards;
+    position = args.position
     getBoard(this)
   });
 
@@ -21,7 +28,7 @@ const getRiver = (v) => {
     function(result){
       if (result.river) {
         river = result.river;
-        v.log(holeCards, board, turn, river)
+        v.log(holeCards, position, board, turn, river, file === null)
       }
   });
 } 
@@ -49,6 +56,59 @@ const getBoard = (v) => {
     function(result){
       if (result.board)
         board = result.board;
-        getTurn(v)
+        loadSolution(v)
   });
-}  
+}
+
+const loadSolution = (v) => {
+  fs.readFile(`./files/${board}.json`, "utf8", (err, jsonString) => {
+    if (err) {
+      console.log("File read failed:", err);
+      return;
+    }
+    file = JSON.parse(jsonString);
+    getStrategy(v)
+  });
+}
+
+const getStrategy = (v) => {
+  const randomNumber = Math.floor(Math.random() * 100) + 1;
+  const strategies = file.strategy.actions;
+  const oopPercentages = file.strategy.strategy[`${holeCards}`];
+  if(position === 0) {
+    const map = new Map()
+    for(let [index, strategy] of strategies.entries()){
+      const percentage = Math.round(oopPercentages[index] * 100)
+      map.set(strategy, percentage)
+      // v.log(`${strategy}: ${percentage}`)
+    }
+    const res = [...map].sort((a,b) => b[1] - a[1])
+
+    for(let result of res){
+      const choice = result[0];
+      const probability = result[1];
+      if(probability > randomNumber) {
+        v.log(chalk.green(`${choice}: ${probability}`))
+        break;
+      }
+    }
+  } else {
+    v.prompt({
+      type: 'list',
+      name: 'villainAction',
+      message: "What did villain Do?",
+      choices: strategies
+      },
+      function(result){
+        if (result.villainAction){
+          const villain = result.villainAction;
+          const ipStrategies = file.childrens[`${villain}`].strategy.actions;
+          const ipPercentages = file.childrens[`${villain}`].strategy.strategy[`${holeCards}`];
+          for(let [index, strategy] of ipStrategies.entries()){
+          v.log(`${strategy}: ${Math.round(ipPercentages[index] * 100)}`)
+          }
+          v.log(randomNumber)
+        }
+    })
+  }
+}
